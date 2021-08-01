7d5ebe38079e6bc6eec4a586c2a2eb1d3be3fd36
AanFurqan <67252822+AanFurqan@users.noreply.github.com>
2021-08-01 20:13:03 +0700

    First Commit

---
+++ app/src/main/java/com/example/thriftuniversecrud/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.example.thriftuniversecrud.Prevalent;
+
+
+import com.example.thriftuniversecrud.UserInformation;
+
+public class Prevalent {
+    public static UserInformation currentOnlineUser;
+
+    public static final String UserFullName = "userFullName";
+    public static final String UserPasswordKey = "UserPassword";
+}
